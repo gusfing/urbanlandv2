c19a7f223dbbf9b9706649a4bde584c0a10abebd
gusfing <gusfing@users.noreply.github.com>
2025-10-07 23:41:22 +0600

    Set Scrollsmoother for smooth scrolling and scroll triggering

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,7 +1,21 @@
 import { Outlet } from "react-router-dom";
+import gsap from "gsap";
+import { ScrollSmoother } from "gsap/all";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Navbar from "../components/Navbar/Navbar";
 
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
 const MainLayout = () => {
+
+    useGSAP(() => {
+        ScrollSmoother.create({
+            smooth: 2,
+            effects: true,
+        });
+    });
+
     return (
         <>
             <Navbar />
