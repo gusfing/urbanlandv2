bd86f656dc87eb2ebaa29e9c0d28ce98300ac0c7
gusfing <gusfing@users.noreply.github.com>
2025-10-09 00:43:45 +0600

    Make hero section mobile responsive

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,43 +1,59 @@
 import gsap from "gsap/all";
 import smoke from "../../assets/smoke_final.mp4";
+import mobileHeroBg from "../../assets/hero-mobile.png"
 import { useGSAP } from "@gsap/react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
 
-    useGSAP(() => {
-        gsap.to(".hero-section .hero-img", {
-            yPercent: 10,
-            scale: 1.1,
-            ease: "power1.inOut",
-            scrollTrigger: {
-                trigger: ".hero-section",
-                start: "top top",
-                end: "bottom top",
-                scrub: 1.5,
-                // markers: true
-            }
-        });
+    const isMobHero = useMediaQuery({
+        query: "(max-width:768px)",
     });
 
+    if (!isMobHero) {
+        useGSAP(() => {
+            gsap.to(".hero-section .hero-img", {
+                yPercent: 10,
+                scale: 1.1,
+                ease: "power1.inOut",
+                scrollTrigger: {
+                    trigger: ".hero-section",
+                    start: "top top",
+                    end: "bottom top",
+                    scrub: 1.5,
+                    // markers: true
+                }
+            });
+        });
+    }
+
     return (
-        <section className="hero-section w-dvw h-dvh p-1 mb-20">
+        <section className="hero-section w-dvw h-dvh md:p-2 p-2.5 mb-20">
             <div className="relative w-full h-full rounded-[2.5rem] overflow-hidden">
+                <div className="responsive-mobile">
+                    {/* Background image (down layer) */}
+                    <div className="hero-img absolute inset-0 bg-[url('./assets/cap1.png')] bg-no-repeat bg-cover bg-center z-0 md:block hidden" />
 
-                {/* Background image (down layer) */}
-                <div className="hero-img absolute inset-0 bg-[url('./assets/cap1.png')] bg-no-repeat bg-cover bg-center z-0"></div>
+                    {/* Mobile image fallback */}
+                    <div className="block lg:hidden mt-10 mb-6">
+                        <img
+                            src={mobileHeroBg}
+                            alt="mobile bg"
+                            className="w-full rounded-[2rem] object-cover shadow-[0_-25px_45px_-10px_rgba(255,0,0,0.15)]"
+                        />
+                    </div>
 
-                {/* Smoke video (upper layer) */}
-                <video
-                    src={smoke}
-                    autoPlay
-                    loop
-                    muted
-                    playsInline
-                    className="absolute inset-0 w-full h-full object-cover z-10 pointer-events-none object-center opacity-50 mix-blend-hard-light "
-                ></video>
-
-                {/* Content class=" absolute object-cover hide-in-safari object-center h-full w-full opacity-0 mix-blend-hard-light  scale-[1.75]"*/}
-                <div className="h-full p-4 flex flex-col justify-center">
+                    {/* Smoke video (upper layer) */}
+                    <video
+                        src={smoke}
+                        autoPlay
+                        loop
+                        muted
+                        playsInline
+                        className="absolute inset-0 md:w-full md:h-full object-cover z-10 pointer-events-none object-center opacity-50 mix-blend-hard-light md:top-0 top-[30%] h-[57%]  rounded-[2rem] md:px-0 px-4"
+                    ></video>
+                </div>
+                <div className="h-full p-4 flex flex-col md:justify-center">
                     <div className="relative">
                         <h1
                             className="text-[#f4efe7] text-start text-6xl mt-10 md:text-9xl font-bold tracking-wider lg:absolute lg:bottom-44 lg:left-2"
@@ -46,9 +62,9 @@ const Hero = () => {
                             Capsules®
                         </h1>
 
-                        <div className="w-full h-auto absolute top-56 flex justify-between items-end">
+                        <div className="w-full h-auto absolute md:top-56 top-30 flex md:flex-row flex-col md:justify-between md:items-end">
                             <h2
-                                className="text-start lg:mt-0 text-[#f4efe7] lg:text-2xl lg:font-bold tracking-wider flex flex-col gap-1"
+                                className="text-start lg:mt-0 md:text-[#f4efe7] text-[#b1a696] text-2xl font-bold md:tracking-wider leading-5 flex flex-col gap-1"
                                 style={{ textShadow: '2px 2px 4px #000' }}
                             >
                                 <span>Closer to</span>
@@ -57,21 +73,12 @@ const Hero = () => {
                             </h2>
 
                             <p
-                                className="w-[20%] text-[#f4efe7] text-[0.7rem] font-normal mt-12  md:font-medium tracking-wide lg:text-end  text-justify"
+                                className="md:w-[20%] w-[80%] text-[#f4efe7] text-[0.7rem] font-bold  md:font-medium tracking-wide lg:text-end mt-2 text-justify"
                                 style={{ textShadow: '2px 2px 4px #000' }}
                             >
                                 Spend unforgettable and remarkable time in the Californian desert with—Capsules.
                             </p>
                         </div>
-                    </div>
-
-                    {/* Mobile image fallback */}
-                    <div className="block lg:hidden mt-10 mb-6">
-                        <img
-                            src="https://capsules.moyra.co/_vercel/image?url=%2Fimages%2Fhero-mobile.png&w=2559&q=80"
-                            alt="mobile bg"
-                            className="w-full rounded-[2rem] object-cover shadow-[0_0_20px_rgba(255,0,0,0.15)]"
-                        />
                     </div>
                 </div>
             </div>
