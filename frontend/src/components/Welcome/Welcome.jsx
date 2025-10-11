6f5d701657469ff76341ccfef2492392a1dd6905
gusfing <gusfing@users.noreply.github.com>
2025-10-11 17:40:37 +0600

    Complete bottom part of a welcome section

--- frontend/src/components/Welcome/Welcome.jsx
+++ frontend/src/components/Welcome/Welcome.jsx
@@ -2,6 +2,8 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap/all";
 import { useMediaQuery } from "react-responsive";
 import { welcomeLinesLG, welcomeLinesSM } from "../../constants/welcome";
+import w1 from "../../assets/welcome-1.png"
+import w2 from "../../assets/welcome-2.png"
 
 const Welcome = () => {
 
@@ -32,9 +34,9 @@ const Welcome = () => {
     });
 
     return (
-        <div className='welcome-section w-full h-dvh text-[#2A2725]'>
+        <div className='welcome-section w-full h-dvh text-[#2A2725]  md:px-7 px-6'>
             <div className='flex flex-col gap-2 tracking-[-4] leading-2'>
-                <div className="w-full md:w-[86%] md:text-[64px] text-[34px] welcome-line md:px-7 px-6 md:py-20">
+                <div className="w-full md:w-[86%] md:text-[64px] text-[34px] welcome-line md:py-20">
                     <div className="w-full welcome-text flex flex-col justify-center items-start">
                         {welcomeLines.map((text, index) => (
                             <span key={index} className="relative block text-darkBrown md:tracking-[-0.010em] tracking-[0.015em]">
@@ -43,6 +45,17 @@ const Welcome = () => {
                             </span>
                         ))}
                     </div>
+                </div>
+            </div>
+            <div className="flex md:flex-row flex-col justify-between items-center md:p-4 md:mt-0 mt-10">
+                <div className="flex flex-row justify-center items-center gap-1">
+                    <img src={w1} alt="welcome image" className="md:rounded-[8rem] rounded-[9rem] md:w-56 w-44" />
+                    <img src={w2} alt="welcome image" className="md:rounded-[8rem] rounded-[9rem] md:w-56 w-44" />
+                </div>
+                <div className="md:w-1/2 w-full md:mt-0 mt-10">
+                    <p className="md:text-[2rem] text-[1.4rem] text-[#b1a696] md:leading-[1.1] md:pr-14 font-normal leading-[26px] tracking-[-0.2px]">
+                        A place where you can be with yourself and your loved ones. A place where you can experience unforgettable desert things.
+                    </p>
                 </div>
             </div>
         </div>
