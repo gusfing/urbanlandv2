fd1e4b73ce0dccf563b9de4ac47118356579e618
gusfing <gusfing@users.noreply.github.com>
2025-10-11 20:26:19 +0600

    Fix rules cards position

--- frontend/src/components/Choose/Choose.jsx
+++ frontend/src/components/Choose/Choose.jsx
@@ -6,16 +6,16 @@ const Choose = () => {
             <div className="mt-10">
                 <h1 className='text-[#f4efe7] text-[9.5rem] leading-[0.9] font-medium tracking-tighter'>Choose the one you like best</h1>
             </div>
-            <div className="w-full flex justify-center items-center gap-10 lg:mt-16">
-                <div className='w-1/2 text-[#b1a696] text-[2rem] md:leading-[1.1] lg:pr-18'>
+            <div className="w-full flex justify-center items-start gap-10 lg:mt-16">
+                <div className='w-1/2 text-[#b1a696] text-[2rem] md:leading-[1.1] lg:pr-16'>
                     <p>You can choose one of three premium capsule houses in our offer. Each of our capsules provides the highest quality and meets the standards adjusted to your needs. Choose the one you like.</p>
                 </div>
                 <div className='w-1/2'>
-                    <div>
-                        <p className="text-[.6rem] text-[#eae5dd] w-[25%]">All Capsules® houses—has built
+                    <div className=" w-[25%]">
+                        <p className="text-[.6rem] text-[#eae5dd]">All Capsules® houses—has built
                             based on the same rules:</p>
                     </div>
-                    <div className="flex flex-1 flex-wrap justify-start items-center gap-2 lg:mt-4">
+                    <div className="flex flex-1 flex-wrap justify-start items-start gap-2 lg:mt-8">
                         <div className="border-[1px] border-[#b1a696] text-[#b1a696] text-[2rem] px-[20px] py-[6px] rounded-full">
                             Sustainable
                         </div>
