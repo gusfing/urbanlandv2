55f05e5ff01d9b5bc7bdacb60160624da422278f
gusfing <gusfing@users.noreply.github.com>
2025-10-11 20:12:36 +0600

    Make choose section

---
+++ frontend/src/components/Choose/Choose.jsx
@@ -0,0 +1,44 @@
+
+const Choose = () => {
+    return (
+        <section className="w-full h-dvh p-8 pt-10 border border-amber-500">
+            <p className='text-[.6rem] text-[#eae5dd]'>Discover Available Capsule<span>®</span></p>
+            <div className="mt-10">
+                <h1 className='text-[#f4efe7] text-[9.5rem] leading-[0.9] font-medium tracking-tighter'>Choose the one you like best</h1>
+            </div>
+            <div className="w-full flex justify-center items-center gap-10 lg:mt-16">
+                <div className='w-1/2 text-[#b1a696] text-[2rem] md:leading-[1.1] lg:pr-18'>
+                    <p>You can choose one of three premium capsule houses in our offer. Each of our capsules provides the highest quality and meets the standards adjusted to your needs. Choose the one you like.</p>
+                </div>
+                <div className='w-1/2'>
+                    <div>
+                        <p className="text-[.6rem] text-[#eae5dd] w-[25%]">All Capsules® houses—has built
+                            based on the same rules:</p>
+                    </div>
+                    <div className="flex flex-1 flex-wrap justify-start items-center gap-2 lg:mt-4">
+                        <div className="border-[1px] border-[#b1a696] text-[#b1a696] text-[2rem] px-[20px] py-[6px] rounded-full">
+                            Sustainable
+                        </div>
+                        <div className="border-[1px] border-[#b1a696] text-[#b1a696] text-[2rem] px-[20px] py-[6px] rounded-full">
+                            Nature—Care
+                        </div>
+                        <div className="border-[1px] border-[#b1a696] text-[#b1a696] text-[2rem] px-[20px] py-[6px] rounded-full">
+                            Smart
+                        </div>
+                        <div className="border-[1px] border-[#b1a696] text-[#b1a696] text-[2rem] px-[20px] py-[6px] rounded-full">
+                            Privacy
+                        </div>
+                        <div className="border-[1px] border-[#b1a696] text-[#b1a696] text-[2rem] px-[20px] py-[6px] rounded-full">
+                            Spacious
+                        </div>
+                        <div className="border-[1px] border-[#b1a696] text-[#b1a696] text-[2rem] px-[20px] py-[6px] rounded-full">
+                            Glassed-in
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Choose;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../../components/Hero/Hero'
 import Welcome from '../../components/Welcome/Welcome'
+import Choose from '../../components/Choose/Choose'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <Welcome />
+      <Choose />
     </div>
   )
 }
