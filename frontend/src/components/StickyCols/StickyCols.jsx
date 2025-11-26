a6af54c998174d2a543d11535f0eb84b6c86e445
gusfing <gusfing@users.noreply.github.com>
2025-11-26 20:53:45 +0600

    Make sticky cols animation

--- frontend/src/components/StickyCols/StickyCols.jsx
+++ frontend/src/components/StickyCols/StickyCols.jsx
@@ -3,8 +3,11 @@ import { useGSAP } from "@gsap/react";
 import colimg1 from "../../assets/cap1-square.jpg";
 import colimg2 from "../../assets/cap2-square.jpg";
 import colimg3 from "../../assets/cap3-square.jpg";
+import { useState } from "react";
 
 const StickyCols = () => {
+
+    const [reveal, setReveal] = useState(false);
 
     useGSAP(() => {
         gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -30,12 +33,13 @@ const StickyCols = () => {
             scrollTrigger: {
                 trigger: ".sticky-cols",
                 start: "top top",
-                end: "+=500%",
+                end: "+=200%",
                 pin: true,
                 scrub: 1,
+                markers: true,
             },
         });
-
+        tl.add(() => setReveal(false));
         // PHASE 1: Reveal col-2, hide col-1
         tl.to(".col-1", { opacity: 0, scale: 0.8, duration: 0.8 })
             .to(".col-2", { x: "0%", duration: 0.8 }, "<")
@@ -45,15 +49,18 @@ const StickyCols = () => {
                 clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
                 duration: 0.8,
             }, "<")
-            .to(".col-img-2 img", { scale: 1, duration: 0.8 }, "<")
+            .to(".col-img-2 img", { scale: 1.6, duration: 0.8 }, "<")
 
-            // PHASE 2: Switch col-2 -> col-3 content
-            .to(".col-2", { opacity: 0, scale: 1, duration: 0.8 })
-            .to(".col-4", { y: "0%", duration: 0.8 }, "<")
+        tl.add(() => setReveal(false));
+        tl.add(() => setReveal(true));
+        // PHASE 2: Switch col-2 -> col-3 content
+        tl.to(".col-2", { opacity: 0, scale: 0.8, duration: 0.8 })
             .to(".col-3 .col-content-wrapper .line span", {
                 yPercent: -125,
                 duration: 0.8,
-            })
+            }, "<")
+        tl.to(".col-3", { x: "0%", duration: 0.8 }, "-=0.8")
+            .to(".col-4", { y: "0%", duration: 0.8 }, "<")
             .to(".col-3 .col-content-wrapper-2 .line span", {
                 yPercent: 0,
                 delay: 0.4,
@@ -66,14 +73,13 @@ const StickyCols = () => {
         };
     });
 
-
     return (
         <section className="sticky-cols w-screen overflow-hidden">
-            <div className="sticky-cols-wrapper">
+            <div className="sticky-cols-wrapper relative w-full h-screen">
                 <div className="col col-1">
                     <div className="col-content">
                         <div className="col-content-wrapper">
-                            <h1 className="text-2xl font-bold leading-1">Enjoy the view
+                            <h1 className="text-2xl text-[#b1a696] font-bold leading-auto">Enjoy the view
                                 <br />
                                 through—the wide
                                 <br />
@@ -81,10 +87,17 @@ const StickyCols = () => {
                                 <br />
                                 window
                             </h1>
-                            <p className="text-md font-medium"> Get closer to the desert nature than ever before
-                                <br />
-                                and admire this unique, breathtaking landscape.
-                            </p>
+                            <div className="col-content-para flex items-center gap-4 justify-between">
+                                <div className="flex items-center gap-0 justify-center">
+                                    <h3 className="border-1 px-3 py-1 rounded-full text-[#aaa091]">1</h3>
+                                    <h3 className="border-1 px-3 py-1 rounded-full text-[#524e4b]">3</h3>
+                                </div>
+                                <p className={`text-[12px] font-medium  ${!reveal ? "mr-6" : "mr-0"}`}> Get closer to the desert nature than ever before
+                                    <br />
+                                    and admire this unique, breathtaking landscape.
+                                </p>
+                            </div>
+
                         </div>
                     </div>
                 </div>
@@ -94,15 +107,15 @@ const StickyCols = () => {
                             <img src={colimg1} alt="img" />
                         </div>
                     </div>
-                    <div className="col col-img-2">
+                    <div className="col col-img-2 p-2">
                         <div className="col-img-wrapper">
-                            <img src={colimg1} alt="img" />
+                            <img src={colimg2} alt="img" />
                         </div>
                     </div>
                 </div>
                 <div className="col col-3">
                     <div className="col-content-wrapper">
-                        <h1 className="text-2xl font-bold leading-1">Enjoy the view
+                        <h1 className="text-2xl font-bold leading-auto">Enjoy the view
                             <br />
                             through—the wide
                             <br />
@@ -110,13 +123,19 @@ const StickyCols = () => {
                             <br />
                             window
                         </h1>
-                        <p className="text-md font-medium"> Get closer to the desert nature than ever before
-                            <br />
-                            and admire this unique, breathtaking landscape.
-                        </p>
+                        <div className={`col-content-para flex items-center gap-4 justify-between ${reveal ? "ml-0" : "ml-6"}`}>
+                            <div className="flex items-center gap-0 justify-center">
+                                <h3 className="border-1 px-3 py-1 rounded-full text-[#aaa091]">{(reveal) ? "3" : "2"}</h3>
+                                <h3 className="border-1 px-3 py-1 rounded-full text-[#524e4b]">3</h3>
+                            </div>
+                            <p className="text-[12px] font-medium"> Get closer to the desert nature than ever before
+                                <br />
+                                and admire this unique, breathtaking landscape.
+                            </p>
+                        </div>
                     </div>
                     <div className="col-content-wrapper-2">
-                        <h1>Enjoy the view
+                        <h1 className="text-2xl font-bold leading-auto">Enjoy the view
                             <br />
                             through—the wide
                             <br />
@@ -124,10 +143,16 @@ const StickyCols = () => {
                             <br />
                             window
                         </h1>
-                        <p> Get closer to the desert nature than ever before
-                            <br />
-                            and admire this unique, breathtaking landscape.
-                        </p>
+                        <div className="col-content-para flex items-center gap-4 justify-between">
+                            <div className="flex items-center gap-0 justify-center">
+                                {/* <h3 className="border-1 px-3 py-1 rounded-full text-[#aaa091]">3</h3>
+                                <h3 className="border-1 px-3 py-1 rounded-full text-[#524e4b]">3</h3> */}
+                            </div>
+                            <p className={`text-[12px] font-medium  ${!reveal ? "mr-0" : "mr-6"}`}> Get closer to the desert nature than ever before
+                                <br />
+                                and admire this unique, breathtaking landscape.
+                            </p>
+                        </div>
                     </div>
                 </div>
                 <div className="col col-4">
