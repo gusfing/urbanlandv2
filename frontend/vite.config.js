83844c7e4f9f469c5acac8befd6c0e8d2b7ebf17
gusfing <gusfing@users.noreply.github.com>
2026-06-11 01:17:18 +0530

    feat: add vite-plugin-image-optimizer and sharp for automatic build-time local image compression

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,31 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [
     tailwindcss(),
-    react()
+    react(),
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+      jpeg: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+      webp: {
+        quality: 80,
+      },
+      avif: {
+        quality: 70,
+      }
+    })
   ],
   optimizeDeps: {
     include: ["gsap"],
